725eb800255af6f7425ddbdab43b4fc4340ae879
Ygor Fonseca <ygor.luiz.fonseca@gmail.com>
2020-10-26 01:37:15 -0300

    Using miragejs Seeds & Updating Endpoints

---
+++ src/server/data/books.js
@@ -0,0 +1,57 @@
+export const books = [
+  {
+    title: 'O Castelo',
+    author: 'Ismail Kadaré',
+    cover: 'https://images-na.ssl-images-amazon.com/images/I/51sajuJifcL._SX318_BO1,204,203,200_.jpg',
+    sinopse: 'Na segunda metade do século XV, o exército otomano, o mais poderoso do mundo, está prestes a atacar um castelo cristão de um pequeno país dos Bálcãs, a Albânia, ponto de interseção entre Ocidente e Oriente. Trata-se da principal cidadela do herói nacional albanês, o legendário George Kastriota-Skanderbeu. Ferozes soldados otomanos galgam escadas colocadas junto às muralhas da fortaleza. Ao redor, o sangue, o fogo, a morte. O paxá Tursum, que comanda as forças de ataque, precisa conquistar a fortaleza se não quiser cair em desgraça junto ao sultão.De dentro do castelo assediado, uma voz anônima narra as privações impostas pelo cerco, entre elas a sede e a fome. A chuva, cuja estação se aproxima, poderá ser a salvação dos castelões e a danação dos atacantes.As rotinas opressivas de um império totalitário são aqui descritas por alguém que viu seu país ser invadido sucessivamente pela Itália fascista, pela Alemanha nazista e pela União Soviética. Sob a rígida censura stalinista da década de 60 - quando Kadaré escreveu o livro -, o foco sobre os séculos em que a Albânia esteve sob o jugo otomano assumiu forte carga libertária.Ismail Kadaré faz uma epopéia moderna, comparável à própria Ilíada de Homero, igualmente centrada no assédio a uma cidadela - Tróia.'
+  },
+  {
+    title: 'Enquanto Agonizo',
+    author: 'Willianm Faulkner',
+    cover: 'https://images-na.ssl-images-amazon.com/images/I/51iaDbslxrL._SX296_BO1,204,203,200_.jpg',
+    sinopse: 'Neste romance, o autor distancia-se da aristocracia sulista americana para falar de gente comum e humilde, como a família Bundren, que se reúne para cumprir o último desejo da matriarca - ser enterrada em Jefferson, ao lado de seus parentes. O marido e os cinco filhos partem com o caixão determinados a cumprir seu objetivo, sem saber como essa viagem mudaria suas vidas.'
+  },
+  {
+    title: 'O Velho e o Mar',
+    author: 'Ernest Hemingway',
+    cover: 'https://images-na.ssl-images-amazon.com/images/I/41fJLiEjr9L._SX334_BO1,204,203,200_.jpg',
+    sinopse: 'Depois de anos na profissão, havia 84 dias que o velho pescador Santiago não apanhava um único peixe. Por isso já diziam se tratar de um salão, ou seja, um azarento da pior espécie. Mas ele possui coragem, acredita em si mesmo, e parte sozinho para alto-mar, munido da certeza de que, desta vez, será bem-sucedido no seu trabalho. Esta é a história de um homem que convive com a solidão, com seus sonhos e pensamentos, sua luta pela sobrevivência e a inabalável confiança na vida. Com um enredo tenso que prende o leitor na ponta da linha, Hemingway escreveu uma das mais belas obras da literatura contemporânea. Uma história dotada de profunda mensagem de fé no homem e em sua capacidade de superar as limitações a que a vida o submete.'
+  },
+  {
+    title: 'Primeiro Amor',
+    author: 'Ivan Truguêniev',
+    cover: 'https://m.media-amazon.com/images/I/41POvTBNYsL.jpg',
+    sinopse: 'Em um passeio por sua casa de veraneio nos arredores de Moscou, o garoto Vladímir Petróvitch, filho único de uma família tradicional, vê uma moça exuberante brincando nos fundos da propriedade. Trata-se de Zinaida, filha de sua vizinha, por quem se apaixonará de forma avassaladora. À medida que eles se aproximam, fica claro quem está no controle da situação. Disposto a tudo para ser correspondido, Vladímir terá de aprend'
+  },
+  {
+    title: 'Dublinenses',
+    author: 'James Joyce',
+    cover: 'https://m.media-amazon.com/images/I/51BmP-ZHwVL.jpg',
+    sinopse: 'Uma das coletâneas de contos mais conhecida da língua inglesa, Dublinenses faz um retrato vívido e inclemente sobre a "boa e velha Dublin" do começo do século XX. Essas quinze histórias, incluindo "Arábias", "Graça" e "Os mortos", mergulham no coração da cidade natal de James Joyce, capturando não só a cadência da fala, mas também o realismo quase brutal dos sentimentos de seus habitantes. A edição ainda inclui a história "O velho vigia", escrita por Berkeley Campbell, que serviu de mote para que Joyce escrevesse o conto "As irmãs", que abre a coletânea. Publicado pela primeira vez em 1914, este livro decifra a vida da classe média católica da Irlanda, mas também lida com temas universais como decepções, frustrações e o despertar sexual. Joyce tinha 25 anos quando escreveu estes contos, considerados por muitos tanto um experimento literário quanto a obra mais acessível do autor.'
+  },
+  {
+    id: '7',
+    title: 'O Feijão e o Sonho',
+    author: 'Oríginess Lessa',
+    cover: 'https://images-na.ssl-images-amazon.com/images/I/411WEnQn4bL._SX345_BO1,204,203,200_.jpg',
+    sinopse: 'O feijão e o sonho narra o relacionamento conflitante entre Campos Lara e sua esposa Maria Rosa, dois seres antagônicos, mas incrivelmente unidos. Ele, professor, escritor com seis livros publicados e intelectual, porém um alienado incapaz de ser um pai e marido comprometido em assumir as mínimas obrigações do dia a dia – moradia, alimentação, vestuário. Maria Rosa, ao contrário, representa o senso prático da vida, o esteio para a família não desmoronar. Um inadaptado. Homem como ele não nascera para o casamento, para a vida do lar. Maria Rosa tinha razão, quase sempre. Ela era o Bom-Senso. Maria Rosa não era uma inimiga. Maria Rosa era o outro lado da vida. O lado em que não daria coisa nenhuma, em que ele sempre fracassaria. O duro. O difícil. O sem cadência nem rima. O do seu permanente naufrágio. Uma história humana e envolvente sobre a difícil arte de conviver.'
+  },
+  {
+    title: 'O Cavaleiro Inexistente',
+    author: 'Italo Calvino',
+    cover: 'https://images-na.ssl-images-amazon.com/images/I/513PV8ZDFpL._SX345_BO1,204,203,200_.jpg',
+    sinopse: 'Nessa espécie de romance de cavalaria às avessas, Italo Calvino põe em marcha toda a sua habilidade construtiva e capacidade alegórica, mas também a sua verve humorística, para contar as aventuras e desventuras de Agilulfo Emo Bertrandino dos Guildiverni e dos Altri de Corbentraz e Sura, o cavaleiro inexistente da corte de Carlos Magno. Circundado por uma galeria de personagens que compõem um verdadeiro exército brancaleônico, Agilulfo serve com fé a causa da cristandade. De permeio, tem de lidar com uma querela em torno de sua nobre reputação, espelhada na armadura trazida sempre imaculadamente alva, embora paradoxalmente vazia. Como nos demais romances da trilogia Nossos antepassados, Calvino mergulha nos tempos heróicos da cavalaria medieval, vistos aqui em chave burlesca, utilizando como narrador uma freira confinada no convento, cuja penitência é justamente escrever a história desse cavaleiro sui generis.'
+  },
+  {
+    title: 'O Visconde Partido ao Meio',
+    author: 'Italo Calvino',
+    cover: 'https://images-na.ssl-images-amazon.com/images/I/51BOlVYkE1L._SX345_BO1,204,203,200_.jpg',
+    sinopse: 'O visconde partido ao meio, publicado originalmente em 1952, veio a compor com O cavaleiro inexistente e O barão nas árvores uma trilogia a que Italo Calvino (1923-85) chamou de Os nossos antepassados, uma espécie de árvore genealógica do homem contemporâneo, alienado, dividido, incompleto. É a história de Medardo di Terralba, o voluntarioso visconde que, na defesa da cristandade contra os turcos, leva um tiro de canhão no peito, mas sobrevive, ficando absurdamente partido ao meio. A metade direita atormentada pela maldade, e a esquerda, pela bondade. "Ainda bem que a bala de canhão dividiu-o apenas em dois", comentam aliviadas suas vítimas.'
+  },
+  {
+    title: 'Uma Vida em Segredo',
+    author: 'Autran Dourado',
+    cover: 'https://images-na.ssl-images-amazon.com/images/I/51igQF0Cl3L._SX335_BO1,204,203,200_.jpg',
+    sinopse: 'Todos os caminhos da obra de Autran Dourado levam a um mesmo e mítico local. Uma geografia que combina a Minas Gerais profunda, onde o autor nasceu em 1926, e o universo tão imaterial e ao mesmo tempo palpável da linguagem, trabalhada das mais variadas formas, do romance ao conto, passando pelo ensaio e a novela. Publicada em 1964, Uma vida em segredo é uma das mais prestigiadas obras do autor. A novela conta a história de Biela, uma moça desajeitada, encolhida e feia, que nasceu e cresceu na roça. Um dia, após a morte do pai, ela chega à cidade, com um vestido antiquado, para viver com o primo Conrado, seu testamenteiro e tutor. Tímida, taciturna, Biela não é apenas uma personalidade discreta, mas alguém que quer preservar sua intimidade, seu mundo pequeno e sereno. Um mundo do qual captamos poucas revelaçőes. Sua vida pacata transcorre, assim, subterrânea, em sua vocaçăo de freira sem claustro, de renúncia às pompas e circunstâncias. Fora um noivado frustrado, muito pouco acontecerá na vida de Biela. Trabalho esmerado de Autran Dourado, recriação inventiva dos modos e linguagem da gente do interior, Uma vida em segredo é uma jóia do novelário brasileiro, frequentemente comparada a Um coração singelo, de Flaubert.'
+  }
+];
